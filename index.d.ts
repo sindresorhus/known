26e76a9b625d1a35eed5b369df5d061008a830a7
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-21 14:37:01 +0000

    Require Node.js 8, add TypeScript definition (#3)

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+/**
+Allow only access to known object properties using [ES2015 `Proxy`](https://ponyfoo.com/articles/es6-proxies-in-depth).
+
+@example
+```
+import known = require('known');
+
+const object = {foo: true};
+
+console.log(object.bar);
+//=> undefined
+
+const object2 = known(object);
+
+// Throws a TypeError when you try to access an unknown property
+console.log(object2.bar);
+//=> [TypeError] Unknown property: bar
+```
+*/
+declare function known<T extends {[key: string]: unknown}>(object: T): T;
+
+export = known;
